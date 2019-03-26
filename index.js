c1a30fa01406e2c8398626072b1d1ebc4947ae42
Jamie Goodnight <goodnightlambda@gmail.com>
2019-03-26 15:13:55 -0400

    Dependencies added. Getting started! Initial commit!

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+const knexConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/lamdba.sqlite3"
+  },
+  debug: true
+};
+
+const db = knex(knexConfig);
 
 // endpoints here
 
