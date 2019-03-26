c2909eddaaf114994591e654d07021a6ba497351
Jamie Goodnight <goodnightlambda@gmail.com>
2019-03-26 18:56:36 -0400

    I had been L A Z Y. I went back and added more conditionals and more robust error messaging. Robust being a relative term, of course!

--- index.js
+++ index.js
@@ -18,15 +18,19 @@ const knexConfig = {
 
 const db = knex(knexConfig);
 
+// --- GET all zoos
+
 server.get("/api/zoos/", (req, res) => {
   db("zoos")
     .then(zoos => {
       res.status(200).json(zoos);
     })
     .catch(err => {
-      res.status(500).json(err);
+      res.status(500).json({ message: "There was a problem retrieving zoos!" });
     });
 });
+
+// --- GET zoo with specified ID
 
 server.get("/api/zoos/:id", (req, res) => {
   zooID = req.params.id;
@@ -34,29 +38,45 @@ server.get("/api/zoos/:id", (req, res) => {
     .first()
     .where({ id: zooID })
     .then(zoo => {
-      res.status(200).json(zoo);
+      if (zoo) {
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ message: "This zoo could not be found!" });
+      }
     })
     .catch(err => {
-      res.status(500).json(err);
+      res
+        .status(500)
+        .json({ message: "There was a problem retrieving this zoo!" });
     });
 });
 
+// --- POST a zoo
+
 server.post("/api/zoos/", (req, res) => {
-  db("zoos")
-    .insert(req.body)
-    .then(ids => {
-      const id = ids[0];
-      db("zoos")
-        .first()
-        .where({ id })
-        .then(zoo => {
-          res.status(200).json(zoo);
-        });
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+  if (req.body.name) {
+    db("zoos")
+      .insert(req.body)
+      .then(ids => {
+        const id = ids[0];
+        db("zoos")
+          .first()
+          .where({ id })
+          .then(zoo => {
+            res.status(200).json(zoo);
+          });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: "There was a problem posting this zoo!" });
+      });
+  } else {
+    res.status(404).json({ message: "Please provide a name for this zoo!" });
+  }
 });
+
+// --- PUT to update existing zoo
 
 server.put("/api/zoos/:id", (req, res) => {
   db("zoos")
@@ -66,13 +86,17 @@ server.put("/api/zoos/:id", (req, res) => {
       if (count > 0) {
         res.status(200).json(count);
       } else {
-        res.status(404).json({ message: "Record not found" });
+        res.status(404).json({ message: "This zoo could not be found!" });
       }
     })
     .catch(err => {
-      res.status(500).json(err);
+      res
+        .status(500)
+        .json({ message: "There was a problem updating this zoo!" });
     });
 });
+
+// --- DELETE a zoo
 
 server.delete("/api/zoos/:id", (req, res) => {
   db("zoos")
@@ -82,11 +106,13 @@ server.delete("/api/zoos/:id", (req, res) => {
       if (count > 0) {
         res.status(204).end();
       } else {
-        res.status(404).json({ message: "Record not found" });
+        res.status(404).json({ message: "This zoo could not be found!" });
       }
     })
     .catch(err => {
-      res.status(500).json(err);
+      res
+        .status(500)
+        .json({ message: "There was a problem deleting this zoo!" });
     });
 });
 
